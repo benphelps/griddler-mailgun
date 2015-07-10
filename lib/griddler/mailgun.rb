e7ff5c57d6620415c7f21e04ef5cb80c14fbf183
Petr Kovar <pejuko@gmail.com>
2015-07-10 11:47:58 +0200

    fix issue #8 - add content_id_map method to email

--- lib/griddler/mailgun.rb
+++ lib/griddler/mailgun.rb
@@ -5,6 +5,12 @@ require 'griddler/mailgun/adapter'
 module Griddler
   module Mailgun
   end
+
+  class Email
+    def content_id_map
+      @params[:content_id_map]
+    end
+  end
 end
 
 Griddler.adapter_registry.register(:mailgun, Griddler::Mailgun::Adapter)
